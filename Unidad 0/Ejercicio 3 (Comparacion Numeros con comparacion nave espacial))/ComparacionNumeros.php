46e0518dc7c8ff5b259f1f7aa8e1f1d6938fb609
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-11-10 13:27:22 +0100

    10/11/2023

--- Unidad 0/Ejercicio 3 (Comparacion Numeros con comparacion nave espacial))/ComparacionNumeros.php
+++ Unidad 0/Ejercicio 3 (Comparacion Numeros con comparacion nave espacial))/ComparacionNumeros.php
@@ -5,11 +5,11 @@ switch ($numero1 <=> $numero2) {
     case 0:
         echo 'El ' . $numero1 . ' es igual que el ' . $numero2;
         break;
-    case -1:
-        echo 'El ' . $numero2 . ' es mayor';
-        break;
     case 1:
         echo 'El ' . $numero1 . ' es mayor';
+        break;
+    case -1:
+        echo 'El ' . $numero2 . ' es mayor';
         break;
 }
 ?>
