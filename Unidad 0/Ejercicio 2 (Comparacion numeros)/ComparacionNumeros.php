fc316c220a09274cb875ddbc5bc739a6520f1e81
MiguelGomezG <miguelgomezgonzalez07@gmail.com>
2023-11-10 13:25:02 +0100

    10/11/2023

---
+++ Unidad 0/Ejercicio 2 (Comparacion numeros)/ComparacionNumeros.php
@@ -0,0 +1,13 @@
+<?php
+$numero1=15;
+$numero2=12;
+if ($numero1>$numero2) {
+    echo 'El '.$numero1.' es mayor';
+}
+if ($numero1<$numero2) {
+    echo 'El '.$numero2.' es mayor';
+}
+if ($numero1==$numero2) {
+    echo 'El '.$numero1.' es igual que el '.$numero2;
+}
+?>
---
+++ Unidad 0/Ejercicio 3 (Comparacion Numeros con comparacion nave espacial))/ComparacionNumeros.php
@@ -0,0 +1,14 @@
+<?php
+$numero1 = 15;
+$numero2 = 12;
+switch ($numero1 <=> $numero2) {
+    case 0:
+        echo 'El ' . $numero1 . ' es igual que el ' . $numero2;
+        break;
+    case -1:
+        echo 'El ' . $numero2 . ' es mayor';
+        break;
+    case 1:
+        echo 'El ' . $numero1 . ' es mayor';
+        break;
+}
